57b7c64add6aac6f7b965b2cf62e2e8e59a3e22e
David <david@uxpertise.ca>
2020-04-19 20:34:58 -0400

    W07D1: Listing Days II

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,20 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spots === 0,
   });
 
+  const formatSpots = (spots) => {
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    } else if (spots === 1) {
+      return `${spots} spot remaining`;
+    } else {
+      return 'no spots remaining';
+    }
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{props.spots}</h3>
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
     </li>
   );
 }
