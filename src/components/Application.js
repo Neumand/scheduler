3d9864110140619d9f0be8de288e95220747c7d1
David <david@uxpertise.ca>
2020-04-27 17:48:34 -0400

    W07D2: The Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import DayList from './DayList';
-import InterviewerList from './InterviewerList';
+import Appointment from './Appointment';
 
 import 'components/Application.scss';
 
@@ -31,6 +31,41 @@ const interviewers = [
   { id: 5, name: 'Sven Jones', avatar: 'https://i.imgur.com/twYrpay.jpg' },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '3pm',
+    interview: {
+      student: 'David Neuman',
+      interviewer: {
+        id: 5,
+        name: 'Sven Jones',
+        avatar: 'https://i.imgur.com/twYrpay.jpg',
+      },
+    },
+  },
+  {
+    id: 4,
+    time: '4pm',
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
   const [interviewer, setInterviewer] = useState(0); // TODO: propose changing this to 'interviewerId'
@@ -54,11 +89,10 @@ export default function Application(props) {
         />
       </section>
       <section className='schedule'>
-        <InterviewerList
-          interviewers={interviewers}
-          value={interviewer} // TODO: propose changing this to 'interviewerId'
-          onChange={setInterviewer}
-        />
+        {appointments.map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment} />;
+        })}
+        <Appointment key='last' time='5pm' />
       </section>
     </main>
   );
